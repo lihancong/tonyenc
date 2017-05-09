e3717a766c4a5ca1f0ab5e570fa356eb549c71f6
Tony <lihancong-tony@foxmail.com>
2017-05-09 10:26:16 +0800

    bug fixed: files can not be handle when the size exceeds 64K in *nix

--- tonyenc.c
+++ tonyenc.c
@@ -164,6 +164,7 @@ PHP_MINFO_FUNCTION(tonyenc)
     php_info_print_table_start();
     php_info_print_table_header(2, "tonyenc support", "enabled");
     php_info_print_table_row(2, "Version", PHP_TONYENC_VERSION);
+    php_info_print_table_row(2, "Open Sourced By", "lihancong.cn");
     php_info_print_table_end();
 
     /* Remove comments if you have entries in php.ini
